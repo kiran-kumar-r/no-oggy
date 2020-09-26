9c0cabee770f3c050321cfd8553fb656e6998526
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-09-26 18:45:14 +0530

    trying to correct no_oggy.cs file

--- no_oggy.cs
+++ no_oggy.cs
@@ -5,7 +5,7 @@ class NoOggyChecker
 {
   static void RemoveOggy(List<string> names)
   {
-    foreach (var name in names)
+    foreach (var name in names )
     {
         if(name.StartsWith("oggy")) {
             Console.WriteLine($"Need to remove {name}!");
